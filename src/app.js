c303f644f18ba31f64a288a577ed1d314a00c8ec
alageev <alageev01@gmail.com>
2020-01-19 00:15:23 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,280 @@
+const path = require(`path`)
+const express = require(`express`)
+const exphbs = require(`express-handlebars`)
+const session = require('express-session')
+const pgp = require("pg-promise")()
+const app = express()
+
+const db = pgp("postgres://postgres:1@127.0.0.1:5432/bingodatabase")
+
+/*TEST AREA*/
+
+
+
+/**/
+
+app.use(session({
+    secret: 'secretWord',   // секретное слово для шифрования
+    key: 'bingoSession',             // имя куки
+    cookie: {
+        path: '/*',          // где действует
+        httpOnly: true,     // чтобы куку не могли читать на клиенте
+        maxAge: null        // время жизни куки
+    },
+    loggedIn: false,
+    nickname: ``,
+    seed: ``,
+    saveUninitialized: false,   // on default
+    resave: false               // on  default
+}));
+
+
+const isLoggedIn = (request, response, next) => {
+    db.query(`select password from bingoschema.users where nickname = '${request.query.nickname}'`)
+        .then((data) => {
+            if(!session.loggedIn) {
+                if (`${request.query.password}` === data[0].password) {
+                    session.loggedIn = true
+                    session.nickname = `${request.query.nickname}`
+                }
+            }
+            return next()
+        })
+        .catch(() => {
+            response.redirect(`/`)
+        });
+}
+
+app.engine(`.hbs`, exphbs({
+    defaultLayout: `BasePage`,
+    extname: `.hbs`,
+    layoutsDir: path.join(__dirname, `/views/`)
+
+}))
+
+app.use(`/scripts`, express.static(__dirname + `/scripts`))
+app.use(`/styles`, express.static(__dirname + `/styles`))
+
+app.set(`view engine`, `.hbs`)
+app.set(`views`, path.join(__dirname, `views/layouts`))
+app.listen(8080)
+
+app.get(`/bingoEdit`, isLoggedIn, (request, response) => {
+    db.query(`select id, name, words from bingoschema.bingos where name = '${request.query.bingoName}'`)
+        .then((data) =>{
+            response.render(`bingoEdit`, {
+                pageName: `Редактировать бинго`,
+                bingoId: data[0].id,
+                bingoName: data[0].name,
+                bingoWords: data[0].words
+            })
+        })
+})
+
+app.get(`/bingoChange`, isLoggedIn, (request, response, next) => {
+    db.query(`update bingoschema.bingos set words = '{${request.query.bingoWords}}', name = '${request.query.bingoName}' where id = '${request.query.bingoId}'`)
+        .then(() => {
+            response.redirect(`/userpage`)
+        })
+})
+
+app.get(`/bingoStart`, isLoggedIn, (request, response) => {
+    let complete = false
+    db.query(`select id from bingoschema.games`)
+        .then((data) => {
+            let gameCode = Math.round(99999.5 + Math.random() * 999999)
+            if (data != ``) {
+                while (!complete) {
+                    for (let code of data[0].id) {
+                        if (gameCode === code) {
+                            gameCode = Math.round(99999.5 + Math.random() * 999999)
+                            break
+                        }
+                    }
+                    complete = true
+                }
+            }
+            db.query(`select id from bingoschema.bingos where name = '${request.query.bingoName}' and author = '${session.nickname}'`)
+                .then((data) => {
+                    let bingo_id = data[0].id
+                    db.query(`select id, players from bingoschema.games where bingo_id = ${data[0].id}`)
+                        .then((data) =>{
+                            let isGame = false
+                            let previousGameCode
+                            if (data != ``) {
+                                for (let i = 0; i < data[0].players.length; i++) {
+                                    if (data[0].players[i] === session.nickname) {
+                                        isGame = true
+                                        previousGameCode = data[0].id
+                                    }
+                                }
+                            }
+                            if (!isGame) {
+                                db.query(`insert into bingoschema.games (id, players, bingo_id) values ('${gameCode}', '{${session.nickname}}', ${bingo_id})`)
+                                    .then(() => {
+                                        response.redirect(`/bingo?gameID=${gameCode}`)
+                                    })
+                                    .catch((error)=>{
+                                        console.log(`insert into bingoschema.games (id, players, bingo_id) values ('${gameCode}', '{${session.nickname}}', ${bingo_id})` + error)
+                                    })
+                            } else {
+                                response.redirect(`/bingo?gameID=${previousGameCode}`)
+                            }
+                        })
+
+                })
+
+        })
+})
+
+app.get(`/tempLogin`, (request, response) => {
+    response.render(`tempLogin`, {
+        pageName: `Временный вход`,
+        gameCode: request.query.gameCode
+    })
+})
+
+app.get(`/tempLoginCheck`, (request, response) => {
+    db.query(`select nickname from bingoschema.users`)
+        .then((data) => {
+            let nicknameIsUsed = false
+            for (let nickname of data[0].nickname){
+                if (nickname === request.query.nickname){
+                    nicknameIsUsed = true
+                }
+            }
+            if (!nicknameIsUsed){
+                session.nickname = request.query.nickname
+                session.loggedIn = true
+                response.redirect(`/gameCode?gameCode=${request.query.gameCode}`)
+            } else {
+                response.redirect(`/tempLogin?gameCode=${request.query.gameCode}`)
+            }
+        })
+})
+
+app.get(`/gameCode`, (request, response) => {
+    if (!session.loggedIn){
+        response.redirect(`/tempLogin?gameCode=${request.query.gameCode}`)
+    } else {
+        db.query(`select players from bingoschema.games where id = '${request.query.gameCode}'`)
+            .then((data) => {
+                let isPlaying = false
+                for (let player of data[0].players){
+                    if (player === session.nickname){
+                        isPlaying = true
+                        break;
+                    }
+                }
+                if (isPlaying){
+                    response.redirect(`/bingo?gameID=${request.query.gameCode}`)
+                } else {
+                    db.query(`update bingoschema.games set players = array_append(players, '${session.nickname}') where id = '${request.query.gameCode}'`)
+                        .then(() => {
+                            response.redirect(`/bingo?gameID=${request.query.gameCode}`)
+                        })
+                }
+            })
+    }
+})
+
+app.get(`/bingo`, isLoggedIn, (request, response) => {
+    db.query(`select bingo_id from bingoschema.games where id = '${request.query.gameID}'`)
+        .then((data) => {
+            db.query(`select name, words from bingoschema.bingos where id = ${data[0].bingo_id}`)
+                .then((data) => {
+                    response.render(`game`, {
+                        pageName: request.query.code,
+                        bingoWords: data[0].words,
+                        bingoName: data[0].name
+                    })
+                })
+        })
+})
+
+app.get(`/login`, (request, response) => {
+    if (!session.loggedIn) {
+        response.render(`login`, {
+            pageName: `Вход`
+        })
+    } else {
+        response.redirect(`/userpage`)
+    }
+})
+
+app.get(`/loginCheck`, isLoggedIn, (request, response) => {
+    response.redirect(`/userpage`)
+})
+
+app.get(`/`, (request, response) => {
+    response.render(`main`, {
+        pageName: `Главная страница`
+    })
+})
+
+app.get(`/register`, (request, response) => {
+    response.render(`register`, {
+        pageName: `Регистрация`,
+    })
+})
+
+app.get(`/registrationCheck`, (request, response) => {
+    db.query(`select * from bingoschema.users where nickname ='${request.query.nickname}'`)
+        .then((data) => {
+            if (data == ``){
+                if (request.query.pass1 === request.query.pass2) {
+                    db.query(`insert into bingoschema.users (nickname, email, password) values ('${request.query.nickname}', '${request.query.email}', '${request.query.pass1}')`)
+                        .then(() => {
+                            console.log(`yes`)
+                            session.nickname = request.query.nickname
+                            session.loggedIn = true
+                            response.redirect(`/userpage`)
+                        })
+                } else {
+                    response.redirect(`/register`)
+                }
+            } else {
+                response.redirect(`/login`)
+            }
+        })
+})
+
+app.get(`/userEdit`, isLoggedIn, (request, response) => {
+    response.render(`userEdit`, {
+        pageName: `Редактировать профиль`,
+    })
+})
+
+app.get(`/userpage`, isLoggedIn, (request, response) => {
+    if (session.loggedIn) {
+        db.query(`select * from bingoschema.bingos where author = '${session.nickname}'`)
+            .then((data)=>{
+                let bingoNames = []
+                for (let i = 0; i < data.length; i++){
+                    bingoNames[i] = data[i].name
+                }
+                response.render(`userpage`, {
+                    pageName: `Мои бинго`,
+                    bingos: bingoNames
+                })
+            })
+    } else {
+        response.redirect(`/`)
+    }
+    })
+
+
+app.get(`/createBingo`, isLoggedIn, (request, response, next) => {
+    db.query(`select max(id) from bingoschema.bingos`)
+        .then((data) => {
+            db.query(`insert into bingoschema.bingos (id, author, words, name) values (${data[0].max + 1}, '${session.nickname}', '{}', 'new_${session.nickname}'s_bingo')`)
+            response.redirect(`/bingoEdit?bingoName=new_${session.nickname}'s_bingo`)
+        })
+})
+
+app.use((err, request, response, next) => {
+    // логирование ошибки, пока просто console.log
+    console.log(err)
+    response.status(500).send(`Something broke!`)
+})
+
---
+++ src/scripts/bingo.js
@@ -0,0 +1,179 @@
+const colors = [
+    `#0a84ff`, //blue
+    `#30d158`, //green
+    `#5e5ce6`, //indigo
+    `#ff9f0a`, //orange
+    `#ff375f`, //pink
+    `#bf5af2`, //purple
+    `#ff453a`, //red
+    `#64d2ff`, //teal
+    `#ffd60a`  //yellow
+]
+
+let marked = Array(25)
+let seed = document.cookie.split(`;`)
+for (let names of seed){
+    if (names.substr(0, cookieName.length - 1) === cookieName){
+        seed = names.substr(cookieName.length + 1)
+    } else {
+        seed = ``
+    }
+}
+console.log(seed)
+
+window.onload = () => {
+    colorCheck(marked)
+}
+
+if (seed === ``){
+    generateBingo()
+} else {
+    newBingo()
+}
+
+for (let i = 0; i < 25; i++){
+    document.getElementById(`td${i}`).style.background = ``
+}
+
+function randomColor(code = Math.round(-0.5 + Math.random() * 9)){
+    return colors[code % 9]
+}
+
+function generateBingo(){
+    let used = Array(phrases.length)
+    for (let i = 0; i < phrases.length; i++){
+        used[i] = false
+    }
+    seed = ``
+    for (let i = 0; i < 5; i++){
+        for (let j = 0; j < 5; j++){
+            let code = Math.round(-0.5 + Math.random() * phrases.length)
+            while (used[code]){
+                code = Math.round(-0.5 + Math.random() * phrases.length)
+            }
+            used[code] = true
+            document.getElementById(`td${5 * i + j}`).innerText = phrases[code]
+            document.getElementById(`td${5 * i + j}`).style.background = ``
+            document.getElementById(`td${5 * i + j}`).style.color = `#000000`
+            seed += `${to62(code)}`
+        }
+    }
+    seed += `00000`
+    document.cookie = `${cookieName}=${seed}`
+}
+
+function newBingo(newSeed){
+    seed = newSeed
+    let colorCode
+    for (let i = 0; i < 25; i++){
+        if (i % 5 === 0){
+            colorCode = to10(newSeed.substr(25 + Math.floor(i / 5), 1))
+        }
+        document.getElementById(`td${i}`).innerText = phrases[to10(seed.substr(i, 1))]
+        if (colorCode % 2 === 1){
+            document.getElementById(`td${i}`).style.color = `#f0f0f0`
+            document.getElementById(`td${i}`).style.background = colors[Math.round(-0.5 + Math.random() * 9)]
+            marked[i] = true
+        } else {
+            document.getElementById(`td${i}`).style.background = ``
+            document.getElementById(`td${i}`).style.color = `#000000`
+            marked[i] = false
+        }
+        colorCode = Math.floor(colorCode / 2)
+    }
+    markedToSeed()
+}
+
+function colorCheck(marked){
+    for (let i = 0; i < 25; i++){
+        if (marked[i]){
+            document.getElementById(`td${i}`).style.color = `#ffffff`
+            document.getElementById(`td${i}`).style.background = randomColor()
+        }
+    }
+}
+
+function updateBingo(i){
+    marked[i] = !marked[i]
+    let isBingo = false
+    if (document.getElementById(`td${i}`).style.background === ``){
+        document.getElementById(`td${i}`).style.background = randomColor()
+        document.getElementById(`td${i}`).style.color = `#ffffff`
+    } else {
+        document.getElementById(`td${i}`).style.background = ``
+        document.getElementById(`td${i}`).style.color = `#000000`
+    }
+    if (
+        marked[Math.floor(i / 5) * 5] &&
+        marked[Math.floor(i / 5) * 5 + 1] &&
+        marked[Math.floor(i / 5) * 5 + 2] &&
+        marked[Math.floor(i / 5) * 5 + 3] &&
+        marked[Math.floor(i / 5) * 5 + 4] ||
+        marked[i % 5] &&
+        marked[i % 5 + 5] &&
+        marked[i % 5 + 10] &&
+        marked[i % 5 + 15] &&
+        marked[i % 5 + 20] ||
+        i % 5 === Math.floor(i / 5) &&
+        marked[0] &&
+        marked[6] &&
+        marked[12] &&
+        marked[18] &&
+        marked[24] ||
+        i % 5 === 4 - Math.floor(i / 5) &&
+        marked[4] &&
+        marked[8] &&
+        marked[12] &&
+        marked[16] &&
+        marked[20]
+    ){
+        isBingo = true
+    }
+    if (isBingo){
+        alert(`BINGO!`)
+    }
+    markedToSeed()
+}
+
+
+
+function to62(number){
+    switch (true){
+        case number < 10:
+            return number
+        case 9 < number && number < 36:
+            return String.fromCodePoint(87 + number)
+        case 35 < number :
+            return String.fromCodePoint(29 + number)
+    }
+}
+
+function to10(number){
+    switch (true){
+        case code(number) <= code(`9`):
+            return code(number) - code(`0`)
+        case code(`a`) <= code(number) && code(number) <= code(`z`):
+            return code(number) - code(`a`) + 10
+        case code(`A`) <= code(number) && code(number) <= code(`Z`):
+            return code(number) - code(`A`) + 36
+    }
+}
+
+function code(number){
+    return String(number).charCodeAt(0)
+}
+
+function markedToSeed(){
+    seed = seed.substr(0, 25)
+    for (let i = 0; i < 5; i++){
+        let result = 0
+        for (let j = 0; j < 5; j++){
+            if (marked[5 * i + j]){
+                result += Math.pow(2, j)
+            }
+        }
+        seed += to62(result)
+    }
+    document.cookie = `${cookieName}=${seed}`
+}
+
