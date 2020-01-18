5178b5e8402bc3c1e1026b51c1061e19e008dc9d
alageev <alageev01@gmail.com>
2020-01-19 00:31:44 +0300

    first check

--- src/app.js
+++ src/app.js
@@ -210,6 +210,7 @@ app.get(`/`, (request, response) => {
     response.render(`main`, {
         pageName: `Главная страница`
     })
+    response.sendStatus(200)
 })
 
 app.get(`/register`, (request, response) => {
