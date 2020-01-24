9262b62159d64c77a6179c846f6de152295f0d6d
alageev <alageev01@gmail.com>
2020-01-24 12:54:06 +0300

    1.0.31 fixed parsing of bingo phrases

--- app/app.js
+++ app/app.js
@@ -4,8 +4,8 @@ const exphbs = require(`express-handlebars`);
 const cookieParser = require('cookie-parser');
 const pgp = require(`pg-promise`)();
 const app = express();
-//const db = pgp(`postgres://nwxuyewwrvrnph:3a4212fd6b652dc571deb79b14561304ad53046d2a192861de74921130c5d95c@ec2-79-125-126-205.eu-west-1.compute.amazonaws.com:5432/d7m188e1rhggl0`);
-const db = pgp("postgres://postgres:1@127.0.0.1:5432/bingodatabase");
+const db = pgp(`postgres://nwxuyewwrvrnph:3a4212fd6b652dc571deb79b14561304ad53046d2a192861de74921130c5d95c@ec2-79-125-126-205.eu-west-1.compute.amazonaws.com:5432/d7m188e1rhggl0`);
+//const db = pgp("postgres://postgres:1@127.0.0.1:5432/bingodatabase");
 
 app.engine(`.hbs`, exphbs({
     defaultLayout: `BasePage`,
